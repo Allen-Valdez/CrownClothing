fbe13589261764d15c36ddc719b514eedfa9e4f7
Allen Valdez <AllenValdez1728@gmail.com>
2022-12-07 18:14:59 -0800

    Added more directory options and used Outlet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Route, Routes, Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './routes/navigation/Navigation';
 import Home from './routes/home/Home';
-
-const Navigation = () => {
-	return (
-		<>
-			<div>
-				<h1>I am the navigation bar</h1>
-			</div>
-			<Outlet />
-		</>
-	);
-};
 
 const Shop = () => {
 	return <h1>I am the shop page</h1>;
